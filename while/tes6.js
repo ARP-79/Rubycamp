01be1a44ce4652b7d482913fa31ec541854e1a8b
andhika <andhikarizki10@gmail.com>
2026-08-04 16:16:57 +0700

    kirim tugas

--- while/tes6.js
+++ while/tes6.js
@@ -7,10 +7,8 @@ let berhasil = false;
 
 // Tulis kode while loop di sini untuk validasi password
 while (percobaan < maxPercobaan) {
-    // Meminta input password dari user (menggunakan prompt jika dijalankan di browser)
     password = prompt("Masukkan password:"); 
     
-    // Anggap password yang benar adalah "admin123" sesuai contoh output
     if (password === "admin123") {
         berhasil = true;
         break; // Keluar dari loop jika login berhasil
