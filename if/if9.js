032547f93c3d7c66c7d1c1b8a3d484da97bf72f2
andhika <andhikarizki10@gmail.com>
2026-08-04 14:30:44 +0700

    kirim tugas

---
+++ if/if9.js
@@ -0,0 +1,28 @@
+let jenisKendaraan = "mobil";
+let durasi = 3; // jam
+
+let tarifPerJam = 0;
+let totalBayar = 0;
+let isValid = true;
+
+// Tulis kode if-else if-else statement di sini
+if (jenisKendaraan === "motor") {
+    tarifPerJam = 2000;
+} else if (jenisKendaraan === "mobil") {
+    tarifPerJam = 5000;
+} else if (jenisKendaraan === "truk") {
+    tarifPerJam = 10000;
+} else {
+    isValid = false;
+    console.log("Kendaraan tidak diizinkan");
+}
+
+// Menghitung total dan menampilkan output jika kendaraan valid
+if (isValid) {
+    totalBayar = tarifPerJam * durasi;
+    
+    console.log(`Jenis kendaraan: ${jenisKendaraan}`);
+    console.log(`Durasi parkir: ${durasi} jam`);
+    console.log(`Tarif per jam: Rp ${tarifPerJam}`);
+    console.log(`Total bayar: Rp ${totalBayar}`);
+}
