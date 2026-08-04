4fb15e14c6a04837bb35378315d347db55f2025a
andhika <andhikarizki10@gmail.com>
2026-08-04 15:02:32 +0700

    kirim tugas

--- for/for7.js
+++ for/for7.js
@@ -1,4 +1,12 @@
 let kata = "programming";
 let jumlahVokal = 0;
+
 // Tulis kode for loop di sini untuk menghitung vokal
+for (let i = 0; i < kata.length; i++) {
+    let huruf = kata.charAt(i).toLowerCase();
+    if (huruf == 'a' || huruf == 'i' || huruf == 'u' || huruf == 'e' || huruf == 'o') {
+        jumlahVokal++;
+    }
+}
+
 console.log("Jumlah vokal: " + jumlahVokal);
