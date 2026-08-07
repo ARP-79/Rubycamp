6b7480ce309d927ce3e620ca595d34ef1fce0315
andhika <andhikarizki10@gmail.com>
2026-08-07 14:42:33 +0700

    kirim tugas

---
+++ string/str8.js
@@ -0,0 +1,5 @@
+// Tulis function cleanAndFormat di sini
+
+console.log(cleanAndFormat("  Hello   World  ", 15));
+console.log(cleanAndFormat("JavaScript", 12));
+console.log(cleanAndFormat("  Code  ", 8));
