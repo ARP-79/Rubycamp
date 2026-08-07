16f2167c5b700fcd6a29d044ebadad0b15d5a330
andhika <andhikarizki10@gmail.com>
2026-08-07 15:28:11 +0700

    kirim tugas

---
+++ function/fung10.js
@@ -0,0 +1,10 @@
+// Tulis function validateAndCalculateGrade di sini
+
+// Test dengan berbagai nilai
+let grade1 = validateAndCalculateGrade("Budi", 85);
+let grade2 = validateAndCalculateGrade("Sari", 150); // invalid
+let grade3 = validateAndCalculateGrade("Andi", 72);
+
+console.log("Grade Budi: " + grade1);
+console.log("Grade Sari: " + grade2);
+console.log("Grade Andi: " + grade3);
---
+++ function/fung9.js
@@ -0,0 +1,6 @@
+// Tulis function processOrder di sini
+
+// Panggil function dan gunakan return value
+let totalBayar1 = processOrder("Nasi Goreng", 2);
+let totalBayar2 = processOrder("Mie Ayam", 3);
+console.log("Total yang harus dibayar: Rp " + (totalBayar1 + totalBayar2));
