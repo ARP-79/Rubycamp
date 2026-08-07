3d5126600c5599b002725d68cfa7fd5b080b5adf
andhika <andhikarizki10@gmail.com>
2026-08-07 14:52:13 +0700

    kirim tugas

---
+++ function/fung3.js
@@ -0,0 +1,8 @@
+// Tulis function greetUser di sini
+function greetUser(nama) {
+    console.log("Halo " + nama + ", selamat datang!");
+}
+
+// Panggil function dengan berbagai nama
+greetUser("Budi");
+greetUser("Sari");
