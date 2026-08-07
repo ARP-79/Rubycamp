b45716f5602985ab71ba85c652e4af0356e2df3e
andhika <andhikarizki10@gmail.com>
2026-08-07 15:13:45 +0700

    kirim tugas

---
+++ function/fung5.js
@@ -0,0 +1,13 @@
+// Tulis function displayUserInfo di sini
+function displayUserInfo(nama,umur,kota){
+    console.log("===info user===");
+    console.log("nama"+ nama);
+    console.log("umur" + umur + "tahun");
+    console.log("kota" + kota);
+    console.log("");
+
+}
+
+// Panggil function dengan data berbeda
+displayUserInfo("Ahmad", 25, "Jakarta");
+displayUserInfo("Lisa", 30, "Bandung");
